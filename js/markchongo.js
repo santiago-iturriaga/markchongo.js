70816201591c13f12d0da40f0166d679805cb6c8
santiago-iturriaga <santiago.iturriaga@gmail.com>
2015-10-19 08:43:52 -0300

    Load pages by url

--- js/markchongo.js
+++ js/markchongo.js
@@ -30,12 +30,52 @@ function loadMainMarkdown(filename) {
 	 
 		// Code to run regardless of success or failure
 		complete: function( xhr, status ) {
+			window.location.hash = "#" + filename;
 		}
 	});
 }
 
 $().ready(function() {
-	loadMainMarkdown("main.md");
+	var page = '';
+	
+	/*var page = window.location.hash;
+	if (page.length > 1) {
+		page = page.substring(1);
+	} else {
+		page = "main.md";
+	}*/
+	
+	/*
+	 * queryParameters -> handles the query string parameters
+	 * queryString -> the query string without the fist '?' character
+	 * re -> the regular expression
+	 * m -> holds the string matching the regular expression
+	 */
+	var queryParameters = {}, queryString = location.search.substring(1),
+		re = /([^&=]+)=([^&]*)/g, m;
+	 
+	// Creates a map with the query string parameters
+	while (m = re.exec(queryString)) {
+		queryParameters[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+	}
+	
+	if (queryParameters['p']) { 
+		page = queryParameters['p'];
+	} else {
+		// Add new parameters or update existing ones
+		queryParameters['p'] = 'main.md';
+		page = 'main.md';
+	}
+	 
+	/*
+	 * Replace the query portion of the URL.
+	 * jQuery.param() -> create a serialized representation of an array or
+	 *     object, suitable for use in a URL query string or Ajax request.
+	 */
+	//location.search = $.param(queryParameters); // Causes page to reload
+	//loc = $.param(queryParameters); // Causes page to reload
+	
+	loadMainMarkdown(page);
 	
 	// Using the core $.ajax() method
 	$.ajax({
